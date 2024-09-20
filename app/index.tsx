e249bada2c0a9b94e74ff48501a93ff030063d54
Mitdd9707 <mit@trentiums.com>
2024-09-20 19:33:08 +0530

    sentry removed, pacakge downgrade

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,6 @@ import React, { useEffect } from 'react';
 import { Redirect, SplashScreen } from 'expo-router';
 SplashScreen.preventAutoHideAsync();
 import { RootState, useSelector } from '@redux/store';
-import * as Sentry from '@sentry/react-native';
-
-Sentry.init({
-  dsn: process.env.EXPO_PUBLIC_SENTRY_DNS,
-  debug: true,
-});
 
 const App = () => {
   const token = useSelector((state: RootState) => state.auth.user.token);
@@ -29,4 +23,4 @@ const App = () => {
   );
 };
 
-export default Sentry.wrap(App);
+export default App;
